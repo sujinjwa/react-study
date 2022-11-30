9f298857eaae1edc345efac9240c9f7c34c92bf6
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-30 11:57:55 +0900

    CSS 임포트 및 동적 데이터 출력하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 10, 30);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
