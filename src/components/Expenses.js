ca7443046739f20626ef371549b0b50ad52bd75d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-12-08 15:59:15 +0900

    feat: 중복된 코드를 Card 컴포넌트로 정의하고, props.children으로 필요한 곳에서 사용

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,30 +1,14 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
-function Expenses(props) {
+function Expenses({ expenses }) {
   return (
-    <div>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </div>
+    <Card className='expenses'>
+      {expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
   );
 }
 
